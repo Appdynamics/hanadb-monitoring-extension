e715029ef938364d4083fea472a3657d429fb42e
michael.englert <michi.eng@gmail.com>
2017-12-13 13:24:08 +0100

    implemented changes to support different username/passwords

--- src/main/java/com/appdynamics/monitors/hanadb/HanaDBMonitor.java
+++ src/main/java/com/appdynamics/monitors/hanadb/HanaDBMonitor.java
@@ -57,14 +57,14 @@ public class HanaDBMonitor extends AManagedMonitor {
             Map<String, ?> config = configuration.getConfigYml();
             if (config != null) {
                 List<Map> queries = (List<Map>) config.get(Globals.queries);
-                ArrayList servers = (ArrayList) config.get(Globals.hosts);
-                if (queries != null && !queries.isEmpty() && servers != null && !servers.isEmpty()) {
-                    for (Map<String, String> server : (Iterable<Map<String, String>>) servers) {
+                ArrayList clusters = (ArrayList) config.get(Globals.clusters);
+                if (queries != null && !queries.isEmpty() && clusters != null && !clusters.isEmpty()) {
+                    for (Map<String, String> cluster : (Iterable<Map<String, String>>) clusters) {
                         for (Map query : queries) {
-                            String password = Utilities.getPassword(config);
-                            String url = config.get(Globals.jdbcPrefix) + server.get(Globals.host) + config.get(Globals.jdbcOptions);
+                            String password = Utilities.getPassword(cluster);
+                            String url = cluster.get(Globals.connectionString);
                             if (logger.isDebugEnabled()) { logger.debug("Connection with JDBC Connection String={}", url); }
-                            JDBCConnectionAdapter jdbcConnectionAdapter = new JDBCConnectionAdapter(url, (String) config.get(Globals.userName), password);
+                            JDBCConnectionAdapter jdbcConnectionAdapter = new JDBCConnectionAdapter(url, cluster.get(Globals.userName), password);
                             HanaDBMonitorTask task = new HanaDBMonitorTask(configuration, jdbcConnectionAdapter, query);
                             configuration.getExecutorService().execute(task);
                         }
--- src/main/java/com/appdynamics/monitors/hanadb/config/Globals.java
+++ src/main/java/com/appdynamics/monitors/hanadb/config/Globals.java
@@ -8,8 +8,7 @@ public class Globals {
     public static final String password = "password";
     public static final String encryptionKey = "encryption-key";
     public static final String passwordEncrypted = "password-encrypted";
-    public static final String jdbcPrefix = "jdbcPrefix";
-    public static final String jdbcOptions = "jdbcOptions";
+    public static final String connectionString = "connectionString";
     public static final String userName = "username";
     public static final String statement = "statement";
     public static final String columns = "columns";
@@ -18,7 +17,7 @@ public class Globals {
     public static final String convertFrom = "convertFrom";
     public static final String convertTo = "convertTo";
     public static final String metric = "metric";
-    public static final String hosts = "hosts";
+    public static final String clusters = "clusters";
     public static final String host = "host";
     public static String jdbcDriverClass = "com.sap.db.jdbc.Driver";
     public static final String driver = "driver";
